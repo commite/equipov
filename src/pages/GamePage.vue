60f763dd1c919285ebba44d9584a44c4f9c416d6
Alejandro Diaz Perez <3balejandro@gmail.com>
2017-12-20 18:54:13 +0100

    Avoid caching current player

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -6,17 +6,17 @@
       </div> -->
     </v-ons-toolbar>
     <div class="turn-layer" v-if="!turnStarted">
-      <div class="center">{{ currentPlayer.name }} turn</div>
+      <div class="center">{{ getCurrentPlayer().name }} turn</div>
       <div class="center"><ons-button @click="startTurn()">Start!</ons-button></div>
     </div>
     <div class="container" v-else>
-      <div class="center">{{ currentPlayer.questions || 0 }}</div>
-      <img :src="currentPlayer.character.pic" :alt="currentPlayer.character.name"/>
-      <div class="center">{{ currentPlayer.name }} turn</div>
+      <div class="center">{{ getCurrentPlayer().questions || 0 }}</div>
+      <img :src="getCurrentPlayer().character.pic" :alt="getCurrentPlayer().character.name"/>
+      <div class="center">{{ getCurrentPlayer().name }} turn</div>
       <div class="button-container">
-        <ons-button @click="wrongQuestion()">Wrong</ons-button>
-        <ons-button @click="guessed()">Guessed</ons-button>
-        <ons-button @click="addQuestion()">Right</ons-button>
+        <ons-button @click="wrongQuestion">Wrong</ons-button>
+        <ons-button @click="guessed">Guessed</ons-button>
+        <ons-button @click="addQuestion">Right</ons-button>
       </div>
     </div>
   </v-ons-page>
@@ -42,37 +42,33 @@ export default {
       set(game) {
        this.$route.params.game = game;
       }
-    },
-    currentPlayer: {
-      get() {
-        return this.game.players[this.currentPlayerIdx];
-      },
-      set(player) {
-        this.game.players[this.currentPlayerIdx] = player;
-      }
     }
   },
   methods: {
     addQuestion() {
-      if(!this.currentPlayer.questions)
-        this.currentPlayer.questions = 0;
-      this.currentPlayer.questions += 1;
+      if(!this.getCurrentPlayer().questions)
+        this.getCurrentPlayer().questions = 0;
+      this.getCurrentPlayer().questions += 1;
       this.$forceUpdate();
+    },
+    getCurrentPlayer() {
+      return this.game.players[this.currentPlayerIdx];
     },
     endGame() {
       console.log("Se acabo");
     },
     guessed() {
       this.turnStarted = false;
-      this.winners.push(this.game.players.splice(this.currentPlayerIdx, 1));
-      if(this.currentPlayerIdx >= this.game.players.length -1)
+      this.winners.push(this.game.players[this.currentPlayerIdx]);
+      this.game.players.splice(this.currentPlayerIdx, 1);
+      if(this.currentPlayerIdx >= this.game.players.length - 1)
         this.currentPlayerIdx = 0;
-      if(this.winners.length == 3)
+      if(this.winners.length == 3 || this.game.players.length == 0)
         this.endGame();
     },
     nextTurn() {
       this.turnStarted = false;
-      if(this.currentPlayerIdx == this.game.players.length -1)
+      if(this.currentPlayerIdx == this.game.players.length - 1)
         this.currentPlayerIdx = 0;
       else
         this.currentPlayerIdx += 1;
